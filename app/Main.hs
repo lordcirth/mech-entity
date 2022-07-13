20890902429275455d1d4be31fd3c0e3c2d0ad37
Nathan Fish <lordcirth@gmail.com>
2022-07-13 15:57:51 -0400

    add Brick

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import           Brick.Main                (defaultMain)
+import           BrickApp
+import           Components ()
+import           InitialState (initialState)
+import           Logic (handleEvent)
+
+import           Control.Monad.Trans.State
+import           System.Random             (mkStdGen, randomRIO)
 import           InitialState
 
 main :: IO ()
