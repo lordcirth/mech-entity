2c6c0aa55ad2b3f72ad174e172e49f584ee244ee
Nathan Fish <lordcirth@gmail.com>
2022-07-13 09:18:36 -0400

    compiles

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Components
 
 main :: IO ()
-main = someFunc
+main = putStrLn $ show w
---
+++ src/Components.hs
@@ -0,0 +1,23 @@
+module Components
+where
+
+import qualified Data.Map.Strict as Map
+
+newtype ID = ID Int
+  deriving (Show, Eq)
+
+data Part = Part {}
+  deriving (Show, Eq)
+data Weapon = Weapon {}
+  deriving (Show, Eq)
+
+data World = World {
+  part :: Map.Map ID Part
+  ,weapons :: Map.Map ID Weapon
+}
+  deriving (Show, Eq)
+
+w = World {
+  part = Map.empty
+  ,weapons = Map.empty
+}
